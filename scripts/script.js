f837425c8c3e39cad82a8468475b81af659b59df
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-08-17 20:55:55 -0400

    new section with two divs and many elements created and added to the HTML

--- scripts/script.js
+++ scripts/script.js
@@ -9,16 +9,18 @@ console.log(signInEl.textContent);
 
 // let featureImgs = document.querySelectorAll("section.features > img");
 // console.log(featureImgs);
+//======================================================================================================
 
 // Let's create a new hero section and new elements and add them to the Hero section
-// Let's create a new div to be included in the section
-let div1 = document.createElement("div");
-console.log(div1)
 
 // First, let's create the section
 let section = document.createElement("section");
 section.className = "hero"
 console.log(section)
+
+// Let's create a new div to be included in the section
+let div1 = document.createElement("div");
+console.log(div1)
 
 //let's create the first element of the section
 let h1 = document.createElement("h1");
@@ -42,12 +44,48 @@ img.width = "400px"
 console.log(img)
 
 // let's add the new elements to div 1
-div1.append(section, h1, p, img);
+div1.append(h1, p, img);
 console.log(div1)
+
+// now append div1 to section as child
+section.append(div1);
+console.log(section)
+
+//======================================================================================================
 
 // Let's create another div for the rest of the following contents
 let div2 = document.createElement("div");
 div2.className = "callToAction"
 console.log(div2)
 
+// Let's create new elements to be included in div2
+let h3 = document.createElement('h3');
+h3.textContent="Give your puppy the gift of companionship and happiness!";
+console.log(h3);
 
+let button = document.createElement('button');
+button.type = "button";
+button.className = "btn btn-info";
+
+let link = document.createElement("a");
+link.href = "./pages/signUp.html";
+link.textContent = "Register Today";
+
+button.appendChild(link);
+div2.appendChild(button)
+console.log(div2);
+
+// now append the new elements to div2
+div2.append(h3,button)
+console.log(div2)
+
+// Now append div2 to section
+section.append(div2);
+console.log(section);
+//======================================================================================================
+
+// Now, let's add the section to the HTML before the features section
+const featuresSection = document.querySelector("section.features")
+console.log(featuresSection);
+document.body.insertBefore(section, featuresSection);
+console.log(document.body)
